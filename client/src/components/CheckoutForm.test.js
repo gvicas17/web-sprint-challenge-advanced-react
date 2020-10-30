ce0965e15d414f860b5ba93b2467dc79fecd0a19
Gabby Vicas <gabbyvicas@Gabbys-MacBook-Pro.local>
2020-10-30 11:47:55 -0500

    met mvp

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -4,7 +4,7 @@ import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {
+test("form header renders",() => {
     render(<CheckoutForm/>)
 
     const header = screen.findByText(/CheckoutForm/i)
@@ -12,24 +12,24 @@ test("form header renders", () => {
     expect(header).toBeTruthy()
 });
 
-test("form shows success message on submit with form details", () => {
+test("form shows success message on submit with form details", async () => {
     render(<CheckoutForm/>)
 
-    const first = screen.findByLabelText(/First Name/i)
-    // const last = screen.findByLabelText(/Last Name/i)
-    // const address = screen.findByLabelText(/Address/i)
-    // const city = screen.findByLabelText(/City/i)
-    // const state = screen.findByLabelText(/State/i)
-    // const zip = screen.findByLabelText(/Zip/i)
-    // const button = screen.findAllByRole('button')
-    // const message = screen.findByText(/You have ordered some plants!/i)
+    const first = screen.queryByLabelText(/First Name/i)
+    const last = screen.queryByLabelText(/Last Name/i)
+    const address = screen.queryByLabelText(/Address/i)
+    const city = screen.queryByLabelText(/City/i)
+    const state = screen.queryByLabelText(/State/i)
+    const zip = screen.queryByLabelText(/Zip/i)
+    const button = screen.getByRole('button')
+    const message = screen.findByTestId('successMessage')
 
     fireEvent.change(first, {target:{name: 'firstName', value: 'Gabby'}})
-    // fireEvent.change(last, {target:{name: 'lastName', value: 'Vicas'}})
-    // fireEvent.change(address, {target:{name: 'address', value: '739 W Briar Pl'}})
-    // fireEvent.change(city, {target:{name:'city', value: 'Chicago'}})
-    // fireEvent.change(state, {target:{name:'state', value: 'IL'}})
-    // fireEvent.change(zip, {target:{name: 'zip', value: '60657'}})
-    // fireEvent.click(button)
-    // expect(message).toBeTruthy()
+    fireEvent.change(last, {target:{name: 'lastName', value: 'Vicas'}})
+    fireEvent.change(address, {target:{name: 'address', value: '739 W Briar Pl'}})
+    fireEvent.change(city, {target:{name:'city', value: 'Chicago'}})
+    fireEvent.change(state, {target:{name:'state', value: 'IL'}})
+    fireEvent.change(zip, {target:{name: 'zip', value: '60657'}})
+    fireEvent.click(button)
+    await expect(message).toBeTruthy()
 });
