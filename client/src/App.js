b5633dad85d24fa54daca82b95da5cc45ae1b7e2
Gabby Vicas <gabbyvicas@Gabbys-MacBook-Pro.local>
2020-10-30 10:26:12 -0500

    test

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ function App() {
   return (
     <div>
       <Router>
-        <nav className="container"><div>Hello</div>
+        <nav className="container">
           <h1>
             React Plants <span role="img">🌿</span>
           </h1>
